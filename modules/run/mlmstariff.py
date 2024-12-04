aec2f5064512e3f97a94d8aabf1fbeef709321f5
Jacek Kotlarski <szumak@virthost.pl>
2024-12-04 13:04:49 +0100

    Template module mlmstariff

---
+++ modules/run/mlmstariff.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+"""
+  mlmstariff.py
+  Author : Jacek 'Szumak' Kotlarski --<szumak@virthost.pl>
+  Created: 4.12.2024, 12:46:01
+  
+  Purpose: LMS module enabling checking of tariff assignment to nodes.
+"""
+
+
+import time
+from inspect import currentframe
+from typing import Dict, List, Optional, Any, Union
+from threading import Thread, Event
+from queue import Queue
+
+from jsktoolbox.basetool.threads import ThBaseObject
+from jsktoolbox.basetool.data import BData
+from jsktoolbox.logstool.logs import LoggerClient, LoggerQueue
+from jsktoolbox.configtool.main import Config as ConfigTool
+from jsktoolbox.attribtool import ReadOnlyClass
+from jsktoolbox.raisetool import Raise
+from jsktoolbox.datetool import Timestamp
+
+from libs.base.classes import BModule
+from libs.interfaces.modules import IRunModule
+from libs.base.classes import BModuleConfig
+from libs.templates.modules import TemplateConfigItem
+from libs.com.message import Message, Multipart, Channel
+from libs.app import AppName
+
+
+class _Keys(object, metaclass=ReadOnlyClass):
+    """Private Keys definition class.
+
+    For internal purpose only.
+    """
+
+
+class _ModuleConf(BModuleConfig):
+    """Module Config private class."""
+
+
+class MLmsTariff(Thread, ThBaseObject, BModule, IRunModule):
+    """Example module."""
+
+    def __init__(
+        self,
+        app_name: AppName,
+        conf: ConfigTool,
+        qlog: LoggerQueue,
+        qcom: Queue,
+        verbose: bool = False,
+        debug: bool = False,
+    ) -> None:
+        """Constructor."""
+        # Thread initialization
+        Thread.__init__(self, name=self._c_name)
+        self._stop_event = Event()
+        self.daemon = True
+        self.sleep_period = 5.0
+
+        # configuration section name
+        self.application = app_name
+        self._section = self._c_name
+        self._cfh = conf
+        self._module_conf = _ModuleConf(self._cfh, self._section)
+
+        # logging level
+        self._bm_debug = debug
+        self._verbose = verbose
+
+        # logger client initialization
+        self.logs = LoggerClient(queue=qlog, name=self._c_name)
+
+        # communication queue
+        self.qcom = qcom
+
+    def _apply_config(self) -> bool:
+        """Apply config from module_conf"""
+        if self.module_conf is None:
+            return False
+
+        try:
+            if self.module_conf.sleep_period:
+                self.sleep_period = self.module_conf.sleep_period
+        except Exception as ex:
+            self.logs.message_critical = f"{ex}"
+            return False
+        return True
+
+    def run(self) -> None:
+        """Main loop."""
+        self.logs.message_notice = "starting..."
+
+        if self.module_conf is None or self.module_conf.message_channel is None:
+            return None
+
+        # initialization local variables
+        channel = Channel(self.module_conf.message_channel)
+
+        # initialization variables from config file
+        if not self._apply_config():
+            self.logs.message_error = "configuration error."
+            return
+
+        if self.debug:
+            self.logs.message_debug = "configuration processing complete"
+            self.logs.message_debug = "entering to the main loop"
+
+        # starting module loop
+        while not self._stopped:
+            # TODO: not implemented
+            # TODO: do something, build a message if necessary, put it in the qcom queue
+
+            # sleep time
+            self.sleep()
+
+        # exiting from loop
+        self.logs.message_notice = "exit"
+
+    def sleep(self) -> None:
+        """Sleep interval for main loop."""
+        sleep_break: float = Timestamp.now() + self.sleep_period
+        while not self._stopped and sleep_break > Timestamp.now():
+            time.sleep(0.2)
+
+    def stop(self) -> None:
+        """Set stop event."""
+        if self.debug:
+            self.logs.message_debug = "stop signal received"
+        if self._stop_event:
+            self._stop_event.set()
+
+    @property
+    def _stopped(self) -> bool:
+        """Return stop flag."""
+        if self._stop_event:
+            return self._stop_event.is_set()
+        return True
+
+    @property
+    def module_stopped(self) -> bool:
+        """Return stop flag."""
+        return self._is_stopped  # type: ignore
+
+    @property
+    def debug(self) -> bool:
+        """Return debug flag."""
+        if self._bm_debug is not None:
+            return self._bm_debug
+        return False
+
+    @property
+    def verbose(self) -> bool:
+        """Return verbose flag."""
+        return self._verbose
+
+    @property
+    def module_conf(self) -> Optional[_ModuleConf]:
+        """Return module conf object."""
+        return self._module_conf  # type: ignore
+
+    @classmethod
+    def template_module_name(cls) -> str:
+        """Return module name for configuration builder."""
+        return cls.__name__.lower()
+
+    @classmethod
+    def template_module_variables(cls) -> List[TemplateConfigItem]:
+        """Return configuration variables template."""
+        out: List[TemplateConfigItem] = []
+        # item format:
+        # TemplateConfigItem()
+        out.append(TemplateConfigItem(desc="Example configuration for module."))
+        out.append(
+            TemplateConfigItem(
+                desc=f"'{_ModuleConf.Keys.SLEEP_PERIOD}' [float], which determines the length of the break"
+            )
+        )
+        out.append(
+            TemplateConfigItem(
+                desc="between subsequent executions of the program's main loop"
+            )
+        )
+        out.append(
+            TemplateConfigItem(
+                desc=f"'{_ModuleConf.Keys.MESSAGE_CHANNEL}' [List[str]], comma separated communication channels list,"
+            )
+        )
+        out.append(
+            TemplateConfigItem(desc="['nr(:default delay=0)'|'nr1:delay', 'nr2:delay']")
+        )
+        out.append(
+            TemplateConfigItem(desc="where 'delay' means the time between generating")
+        )
+        out.append(
+            TemplateConfigItem(
+                desc="subsequent notifications for a given channel and can be given in"
+            )
+        )
+        out.append(
+            TemplateConfigItem(
+                desc="seconds or a numerical value with the suffix 's|m|h|d|w'"
+            )
+        )
+        out.append(
+            TemplateConfigItem(
+                varname=_ModuleConf.Keys.SLEEP_PERIOD, value=3.25, desc="[second]"
+            )
+        )
+        out.append(
+            TemplateConfigItem(
+                varname=_ModuleConf.Keys.MESSAGE_CHANNEL,
+                value=["1"],
+            )
+        )
+        return out
+
+
+# #[EOF]#######################################################################
