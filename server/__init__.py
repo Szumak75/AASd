49227dbbcdbac155ab077f4fdb95de1ceeb10c3f
Jacek 'Szumak' Kotlarski <szumak@virthost.pl>
2026-03-25 23:11:30 +0100

    fix: restore development suffix in versioning

--- server/__init__.py
+++ server/__init__.py
@@ -5,6 +5,6 @@ from typing import Tuple
 
 __author__ = "Jacek 'Szumak' Kotlarski"
 __version_info__: Tuple[int, int, int] = (2, 4, 6)
-# __suffix__: str = "-DEV"
-__suffix__: str = ""
+__suffix__: str = "-DEV"
+# __suffix__: str = ""
 __version__: str = ".".join(map(str, __version_info__)) + __suffix__
