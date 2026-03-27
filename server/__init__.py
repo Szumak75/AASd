2efd6eeb7e56878170a5558faf0c58b57c444d67
Jacek 'Szumak' Kotlarski <szumak@virthost.pl>
2026-03-27 12:51:50 +0100

    feat: comment out development suffix in versioning for clarity

--- server/__init__.py
+++ server/__init__.py
@@ -6,5 +6,5 @@ from typing import Tuple
 __author__ = "Jacek 'Szumak' Kotlarski"
 __version_info__: Tuple[int, int, int] = (2, 4, 14)
 __suffix__: str = ""
-__suffix__: str = "-DEV"
+# __suffix__: str = "-DEV"
 __version__: str = ".".join(map(str, __version_info__)) + __suffix__
